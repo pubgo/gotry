681cd41fe56ba3908c5400ae4a3a749176bd50a7
白云辉 <baiyunhui@yuanben.org>
2019-05-09 04:04:22 +0800

    delete other func

--- try.go
+++ try.go
@@ -24,7 +24,7 @@ func (t *_try) Then(fn interface{}) *_try {
 	assert.ST(_fn.Type().NumIn() != len(t._values), "the params num is not match")
 
 	_t := &_try{}
-	_t.err = _Try(func() {
+	_t.err = assert.KTry(func() {
 		_t._values = _fn.Call(t._values)
 	})
 
@@ -83,57 +83,10 @@ func (t *_try) KErr() *assert.KErr {
 	return t.err.(*assert.KErr)
 }
 
-func Try(f interface{}, params ...interface{}) *_try {
-	t := reflect.TypeOf(f)
-	assert.ST(t.Kind() != reflect.Func, "the params is not func type")
-
+func Try(f interface{}, args ...interface{}) *_try {
 	_t := &_try{}
-	_t.err = _Try(func() {
-		var vs []reflect.Value
-		for i, p := range params {
-			if p == nil {
-				if t.IsVariadic() {
-					i = 0
-				}
-
-				vs = append(vs, reflect.New(t.In(i)).Elem())
-			} else {
-				vs = append(vs, reflect.ValueOf(p))
-			}
-		}
-		_t._values = reflect.ValueOf(f).Call(vs)
+	_t.err = assert.KTry(func() {
+		_t._values = assert.FnOf(f, args...)()
 	})
 	return _t
-}
-
-func _Try(fn func()) (err error) {
-	assert.ST(fn == nil, "the func is nil")
-
-	_v := reflect.TypeOf(fn)
-	assert.ST(_v.Kind() != reflect.Func, "the params type(%s) is not func", _v.String())
-
-	defer func() {
-		defer func() {
-			m := &assert.KErr{}
-			if r := recover(); r != nil {
-				switch d := r.(type) {
-				case *assert.KErr:
-					m = d
-				case error:
-					m.Err = d
-					m.Msg = d.Error()
-				default:
-					panic("type error, must be *assert.KErr type")
-				}
-			}
-
-			if m.Err == nil {
-				err = nil
-			} else {
-				err = m
-			}
-		}()
-		reflect.ValueOf(fn).Call([]reflect.Value{})
-	}()
-	return
 }
